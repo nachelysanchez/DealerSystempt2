802841d26d3d99d4f9c48ebd035934c9ec56927a
Nachely Sánchez <nashely-5@hotmail.com>
2022-09-24 00:47:39 -0400

    Agregando el registro de usuarios totalmente funcional

---
+++ Scripts/Scripts.sql
@@ -0,0 +1,23 @@
+﻿ALTER PROCEDURE sp_InsertarUsuarios 
+@Id int, @Nombre varchar(100), @Usuario varchar(100), @Pass varchar(max), @tipo int, @activo bit
+AS
+	INSERT INTO dbo.Usuarios VALUES (@Nombre, @Usuario, @Pass, @tipo, @activo)
+GO
+
+CREATE PROCEDURE sp_ModificarUsuarios 
+@Id int, @Nombre varchar(100), @Usuario varchar(100), @Pass varchar(max), @tipo int, @activo bit
+AS
+	UPDATE dbo.Usuarios SET Nombre = @Nombre, Usuario = @Usuario, Pass = @Pass, Tipo = @tipo, Estado = @activo WHERE ID = @Id
+GO
+
+CREATE PROCEDURE sp_EliminarUsuarios 
+@Id int
+AS
+	DELETE FROM dbo.Usuarios WHERE ID = @Id
+GO
+
+CREATE PROCEDURE sp_BuscarUsuarios
+@Id int
+AS
+	SELECT * FROM dbo.Usuarios WHERE ID = @Id
+GO
--- UI/Registros/rUsuarios.Designer.cs
+++ UI/Registros/rUsuarios.Designer.cs
@@ -29,19 +29,306 @@ namespace DealerSystempt2.UI.Registros
         /// </summary>
         private void InitializeComponent()
         {
+            this.tipoCmb = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Usertxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Nombretxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Idtxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Passtxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ActivoRadio = new System.Windows.Forms.CheckBox();
+            this.CancelarButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // tipoCmb
+            // 
+            this.tipoCmb.FormattingEnabled = true;
+            this.tipoCmb.Items.AddRange(new object[] {
+            "Administrador",
+            "Usuario"});
+            this.tipoCmb.Location = new System.Drawing.Point(207, 295);
+            this.tipoCmb.Name = "tipoCmb";
+            this.tipoCmb.Size = new System.Drawing.Size(512, 24);
+            this.tipoCmb.TabIndex = 75;
+            this.tipoCmb.Text = "Seleccione";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(63, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 18);
+            this.label5.TabIndex = 74;
+            this.label5.Text = "Contraseña";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnBuscar.BackColor = System.Drawing.Color.Salmon;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.Location = new System.Drawing.Point(628, 91);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(91, 36);
+            this.btnBuscar.TabIndex = 73;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnEliminar.BackColor = System.Drawing.Color.Red;
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar.Location = new System.Drawing.Point(470, 369);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(103, 36);
+            this.btnEliminar.TabIndex = 72;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnInsertar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnInsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsertar.ForeColor = System.Drawing.Color.White;
+            this.btnInsertar.Location = new System.Drawing.Point(272, 369);
+            this.btnInsertar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(91, 36);
+            this.btnInsertar.TabIndex = 71;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = false;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnModificar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.ForeColor = System.Drawing.Color.White;
+            this.btnModificar.Location = new System.Drawing.Point(364, 369);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(104, 36);
+            this.btnModificar.TabIndex = 70;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnNuevo.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnNuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.ForeColor = System.Drawing.Color.White;
+            this.btnNuevo.Location = new System.Drawing.Point(177, 369);
+            this.btnNuevo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(91, 36);
+            this.btnNuevo.TabIndex = 69;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = false;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.label4.Location = new System.Drawing.Point(252, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(265, 43);
+            this.label4.TabIndex = 68;
+            this.label4.Text = "Registro de Usuarios";
+            // 
+            // Usertxt
+            // 
+            this.Usertxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Usertxt.Location = new System.Drawing.Point(207, 198);
+            this.Usertxt.Name = "Usertxt";
+            this.Usertxt.Size = new System.Drawing.Size(511, 22);
+            this.Usertxt.TabIndex = 67;
+            this.Usertxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(63, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 18);
+            this.label3.TabIndex = 66;
+            this.label3.Text = "Usuario";
+            // 
+            // Nombretxt
+            // 
+            this.Nombretxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Nombretxt.Location = new System.Drawing.Point(207, 149);
+            this.Nombretxt.Name = "Nombretxt";
+            this.Nombretxt.Size = new System.Drawing.Size(511, 22);
+            this.Nombretxt.TabIndex = 65;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(63, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 18);
+            this.label2.TabIndex = 64;
+            this.label2.Text = "Nombre";
+            // 
+            // Idtxt
+            // 
+            this.Idtxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Idtxt.Location = new System.Drawing.Point(207, 98);
+            this.Idtxt.Name = "Idtxt";
+            this.Idtxt.Size = new System.Drawing.Size(415, 22);
+            this.Idtxt.TabIndex = 63;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 18);
+            this.label1.TabIndex = 62;
+            this.label1.Text = "Usuario Id";
+            // 
+            // Passtxt
+            // 
+            this.Passtxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Passtxt.Location = new System.Drawing.Point(207, 246);
+            this.Passtxt.Name = "Passtxt";
+            this.Passtxt.PasswordChar = '*';
+            this.Passtxt.Size = new System.Drawing.Size(511, 22);
+            this.Passtxt.TabIndex = 76;
+            this.Passtxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(63, 296);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 18);
+            this.label6.TabIndex = 77;
+            this.label6.Text = "Tipo";
+            // 
+            // ActivoRadio
+            // 
+            this.ActivoRadio.AutoSize = true;
+            this.ActivoRadio.Location = new System.Drawing.Point(650, 65);
+            this.ActivoRadio.Name = "ActivoRadio";
+            this.ActivoRadio.Size = new System.Drawing.Size(68, 21);
+            this.ActivoRadio.TabIndex = 79;
+            this.ActivoRadio.Text = "Activo";
+            this.ActivoRadio.UseVisualStyleBackColor = true;
+            // 
+            // CancelarButton
+            // 
+            this.CancelarButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.CancelarButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.CancelarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelarButton.ForeColor = System.Drawing.Color.White;
+            this.CancelarButton.Location = new System.Drawing.Point(579, 369);
+            this.CancelarButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CancelarButton.Name = "CancelarButton";
+            this.CancelarButton.Size = new System.Drawing.Size(103, 36);
+            this.CancelarButton.TabIndex = 80;
+            this.CancelarButton.Text = "Cancelar";
+            this.CancelarButton.UseVisualStyleBackColor = false;
+            this.CancelarButton.Click += new System.EventHandler(this.CancelarButton_Click);
             // 
             // rUsuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CancelarButton);
+            this.Controls.Add(this.ActivoRadio);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Passtxt);
+            this.Controls.Add(this.tipoCmb);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Usertxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Nombretxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Idtxt);
+            this.Controls.Add(this.label1);
             this.Name = "rUsuarios";
             this.Text = "Registros de Usuarios";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox tipoCmb;
+        private System.Windows.Forms.Label label5;
+        protected System.Windows.Forms.Button btnBuscar;
+        protected System.Windows.Forms.Button btnEliminar;
+        protected System.Windows.Forms.Button btnInsertar;
+        protected System.Windows.Forms.Button btnModificar;
+        protected System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Usertxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Nombretxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Idtxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Passtxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox ActivoRadio;
+        protected System.Windows.Forms.Button CancelarButton;
     }
 }
