4eb8d6ac37bde34a52653397a46bacea2d5d9d89
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2022-09-24 19:52:44 -0400

    Ventas

--- UI/Registros/rVentas.cs
+++ UI/Registros/rVentas.cs
@@ -16,5 +16,10 @@ namespace DealerSystempt2.UI.Registros
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
