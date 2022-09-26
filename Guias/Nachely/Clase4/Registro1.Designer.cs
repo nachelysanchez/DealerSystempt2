9eb23541e8cc3890f9c37530676b978a52d0aff1
Nachely Sánchez <nashely-5@hotmail.com>
2022-09-26 01:05:26 -0400

    Subiendo guia 4

---
+++ Guias/Nachely/Clase4/Registro1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace DealerSystempt2.Guias.Nachely.Clase4
+{
+    partial class Registro1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVolverC = new System.Windows.Forms.Button();
+            this.btnLimpiarC = new System.Windows.Forms.Button();
+            this.btnIngresarC = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVolverC
+            // 
+            this.btnVolverC.Location = new System.Drawing.Point(272, 164);
+            this.btnVolverC.Name = "btnVolverC";
+            this.btnVolverC.Size = new System.Drawing.Size(75, 40);
+            this.btnVolverC.TabIndex = 11;
+            this.btnVolverC.Text = "Volver";
+            this.btnVolverC.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiarC
+            // 
+            this.btnLimpiarC.Location = new System.Drawing.Point(164, 164);
+            this.btnLimpiarC.Name = "btnLimpiarC";
+            this.btnLimpiarC.Size = new System.Drawing.Size(75, 40);
+            this.btnLimpiarC.TabIndex = 10;
+            this.btnLimpiarC.Text = "Limpiar";
+            this.btnLimpiarC.UseVisualStyleBackColor = true;
+            this.btnLimpiarC.Click += new System.EventHandler(this.btnLimpiarC_Click);
+            // 
+            // btnIngresarC
+            // 
+            this.btnIngresarC.Location = new System.Drawing.Point(45, 164);
+            this.btnIngresarC.Name = "btnIngresarC";
+            this.btnIngresarC.Size = new System.Drawing.Size(75, 40);
+            this.btnIngresarC.TabIndex = 9;
+            this.btnIngresarC.Text = "Ingresar";
+            this.btnIngresarC.UseVisualStyleBackColor = true;
+            this.btnIngresarC.Click += new System.EventHandler(this.btnIngresarC_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(143, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Clientes";
+            // 
+            // Registro1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 268);
+            this.Controls.Add(this.btnVolverC);
+            this.Controls.Add(this.btnLimpiarC);
+            this.Controls.Add(this.btnIngresarC);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Registro1";
+            this.Text = "Registro1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVolverC;
+        private System.Windows.Forms.Button btnLimpiarC;
+        private System.Windows.Forms.Button btnIngresarC;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Guias/Nachely/Clase4/Registro1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DealerSystempt2.Guias.Nachely.Clase4
+{
+    public partial class Registro1 : Form
+    {
+        private SqlConnection conn;
+        private SqlCommand insert1;
+        private string sCn;
+        public Registro1()
+        {
+            InitializeComponent();
+            Conexion conexion = new Conexion();
+            conexion.conec();
+            sCn = conexion.cadena;
+            conn = new SqlConnection(sCn);
+        }
+
+        private void btnIngresarC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                string insert;
+                insert = "INSERT INTO CLIENTES VALUES ('" + textBox1.Text + "')";
+                insert1 = new SqlCommand(insert, conn);
+
+                insert1.ExecuteNonQuery();
+                textBox1.Clear();
+                MessageBox.Show("Agregado exitosamente");
+                conn.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnLimpiarC_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+    }
+}
